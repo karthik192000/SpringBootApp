ad1c12bc33782a29e4fb27ab52706358e49b921e
Karthik Bankapur <bankapurkarthik@gmail.com>
2025-02-16 22:22:55 +0530

    Added New API

---
+++ src/main/java/com/spring/eureka/server/application/controller/RestControllerClass.java
@@ -0,0 +1,20 @@
+package com.spring.eureka.server.application.controller;
+
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerClass {
+
+
+
+
+    @GetMapping(value = "/v1/greet",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> greet(){
+        return new ResponseEntity<>("hello this is spring boot application running on ecs", HttpStatusCode.valueOf(200));
+    }
+}
